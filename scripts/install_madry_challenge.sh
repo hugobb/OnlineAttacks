318efa2417742a26eb491d97ebdf424a135c9376
Hugo Berard <hberard@fb.com>
2021-08-30 14:27:40 -0400

    Update install_madry_challenge.sh

--- scripts/install_madry_challenge.sh
+++ scripts/install_madry_challenge.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
+MODELS_PATH=$1
 CURRENT_PATH=$(pwd)
-MODELS_PATH=/checkpoint/hberard/OnlineAttack/pretained_models
-MADRY_MNIST_PATH=online_attacks/classifiers/madry/madry_mnist
-MADRY_CIFAR_PATH=online_attacks/classifiers/madry/madry_cifar
+MADRY_PATH=online_attacks/classifiers/madry
 
 ### INSTALLING MADRY MNIST CHALLENGE ###
-git clone https://github.com/MadryLab/mnist_challenge $MADRY_MNIST_PATH
+git clone https://github.com/MadryLab/mnist_challenge $MADRY_PATH/madry_mnist
 mkdir /tmp/madry_mnist
 cd /tmp/madry_mnist
-python $CURRENT_PATH/$MADRY_MNIST_PATH/fetch_model.py secret
-python $CURRENT_PATH/$MADRY_MNIST_PATH/fetch_model.py natural
-python $CURRENT_PATH/$MADRY_MNIST_PATH/fetch_model.py adv_trained
+python -m online_attacks.classifiers.madry.madry_mnist.fetch_model secret
+python -m online_attacks.classifiers.madry.madry_mnist.fetch_model natural
+python -m online_attacks.classifiers.madry.madry_mnist.fetch_model adv_trained
 mkdir $MODELS_PATH/mnist/madry
 mv models/* $MODELS_PATH/mnist/madry
 cd $CURRENT_PATH
 rm -rf /tmp/madry_mnist
 
 ### INSTALLING MADRY CIFAR CHALLENGE ###
-git clone https://github.com/MadryLab/cifar10_challenge $MADRY_CIFAR_PATH
+git clone https://github.com/MadryLab/cifar10_challenge $MADRY_PATH/madry_cifar
 mkdir /tmp/madry_cifar
 cd /tmp/madry_cifar
-python $CURRENT_PATH/$MADRY_CIFAR_PATH/fetch_model.py secret
-python $CURRENT_PATH/$MADRY_CIFAR_PATH/fetch_model.py natural
-python $CURRENT_PATH/$MADRY_CIFAR_PATH/fetch_model.py adv_trained
+python online_attacks.classifiers.madry.madry_cifar.fetch_model secret
+python online_attacks.classifiers.madry.madry_cifar.fetch_model natural
+python online_attacks.classifiers.madry.madry_cifar.fetch_model adv_trained
 
 mv models/model_0 models/secret
 mv models/naturally_trained models/natural
